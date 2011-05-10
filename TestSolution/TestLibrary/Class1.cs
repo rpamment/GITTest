68d19132cbdcd4c8ce05659992d30187a2152204
unknown <Robin.Pamment@.SRH.local>
2011-05-10 21:21:16 +0100

    First test commit

---
+++ TestSolution/TestLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestLibrary
+{
+    public class TestUtility
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("Done stuff");
+        }
+    }
+}
